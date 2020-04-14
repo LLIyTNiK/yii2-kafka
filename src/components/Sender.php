db03df560eed48c93854184ba91b4c6edf9606d9
fynjy <antony.ets@yandex.ru>
2020-04-14 14:44:14 +0300

    test

--- src/components/Sender.php
+++ src/components/Sender.php
@@ -9,6 +9,7 @@ class Sender extends Kafka
     public $producer;
     public $topic;
     public $resendNum = 5;
+	public $flushTimeout = 100;
 
     public function init(){
         parent::init();
@@ -21,7 +22,7 @@ class Sender extends Kafka
         $this->topic->produce(RD_KAFKA_PARTITION_UA,0,json_encode($message));
         $this->producer->poll(0);
         for ($flushRetries = 0; $flushRetries < $this->resendNum; $flushRetries++) {
-            $result = $this->producer->flush(10000);
+            $result = $this->producer->flush($this->flushTimeout);
             if (RD_KAFKA_RESP_ERR_NO_ERROR === $result) {
                 break;
             }
