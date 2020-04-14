9e23d0cc6371825601b777479d6d5cd563e8e712
fynjy <antony.ets@yandex.ru>
2020-04-14 14:18:30 +0300

    test

---
+++ src/Components/Sender.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace lliytnik\kafka\components;
+
+
+class Sender extends Kafka
+{
+    public $producer;
+    public $topic;
+    public $resendNum = 5;
+
+    public function init(){
+        parent::init();
+        $this->producer = new \RdKafka\Producer($this->_conf);
+        $this->producer->addBrokers($this->brokers);
+        $this->topic = $this->producer->newTopic($this->topicName);
+    }
+
+    public function send($message){
+        $this->topic->produce(RD_KAFKA_PARTITION_UA,0,json_encode($message));
+        $this->producer->poll(0);
+        for ($flushRetries = 0; $flushRetries < $this->resendNum; $flushRetries++) {
+            $result = $this->producer->flush(10000);
+            if (RD_KAFKA_RESP_ERR_NO_ERROR === $result) {
+                break;
+            }
+        }
+        if (RD_KAFKA_RESP_ERR_NO_ERROR !== $result) {
+            return false;
+        }
+        return true;
+    }
+}
